1a646b2b4fca1f791b1324d68157322568c31fab
Jabar <rrkdev5@gmail.com>
2025-05-03 18:47:49 +0700

    Add initial with starter kit livewire

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,16 +1,15 @@
 <?php
 
-namespace tests\Feature;
+namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    use RefreshDatabase;
+
+    public function test_returns_a_successful_response(): void
     {
         $response = $this->get('/');
 
